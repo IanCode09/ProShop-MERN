22aaa6dbdd805170fb43fed36e39afed7f782fa8
IanCode09 <ianlombu@protonmail.com>
2020-10-05 23:37:36 +0700

    Create Rating Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,9 +9,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             <Row>
-                {products.map(item => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={item} />
+                {products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
                     </Col>
                 ))}
             </Row>
