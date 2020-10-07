d87e7c198b192be1b3223f809d15e8921af831b4
IanCode09 <ianlombu@protonmail.com>
2020-10-07 14:57:20 +0700

    Bringing Redux State Into HomeScreen -useDispatch & useSelector

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -9,9 +9,9 @@ export const productListReducers = (state = { products: [] }, action) => {
         case PRODUCT_LIST_REQUEST:
             return { loading: true, products: [] }
         case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: action.playload }
+            return { loading: false, products: action.payload }
         case PRODUCT_LIST_FAIL:
-            return { loading: false, error: action.playload }
+            return { loading: false, error: action.payload }
         default:
             return state
     }
