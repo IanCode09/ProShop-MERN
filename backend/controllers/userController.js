8cb6179c97309c63660b6eced5ca03e3ac6f48ef
IanCode09 <ianlombu@protonmail.com>
2020-10-11 19:15:31 +0700

    User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,40 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 
+// @desc   Register a new User
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if(userExist) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name,
+        email, 
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
+})
+
+
 // @desc    Get User Profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -45,4 +79,8 @@ const getUserProfile = asyncHandler(async (req, res) => {
    }
 })
 
-export { authUser, getUserProfile }
+
+
+
+
+export { authUser, registerUser, getUserProfile }
